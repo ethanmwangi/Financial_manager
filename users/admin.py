a4940537032c74551e8915382fdce6b5b6f37844
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-08-20 19:26:37 +0300

    made transactions reflect on the admin side of  django

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Transaction
+
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['username', 'email', 'salary', 'financial_goal', 'is_staff']
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'transaction_type', 'date')
+    list_filter = ('transaction_type', 'date')
+    search_fields = ('user__username', 'description')
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     # Extra fields we might use later
@@ -8,3 +9,15 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (('income', 'Income'), ('expense', 'Expense'))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    category = models.CharField(max_length=100, blank=True)
+    date = models.DateField(auto_now_add=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.transaction_type.capitalize()} - {self.amount}"
