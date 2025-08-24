c04c6f58c33bc8b430a56b822eac2f44a435ee7f
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-08-24 23:21:01 +0300

    added category in the tranaction form

--- transactions/forms.py
+++ transactions/forms.py
@@ -5,7 +5,7 @@ from .models import Transaction
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = ['title', 'amount', 'transaction_type', 'date', 'description']
+        fields = ['title', 'amount', 'transaction_type', 'date', 'description',]
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
         }
--- transactions/models.py
+++ transactions/models.py
@@ -6,13 +6,21 @@ class Transaction(models.Model):
         ('INCOME', 'Income'),
         ('EXPENSE', 'Expense'),
     ]
-
+    CATEGORY_CHOICES = [
+    ('food', 'Food'),
+    ('transport', 'Transport'),
+    ('shopping', 'Shopping'),
+    ('bills', 'Bills'),
+    ('other', 'Other'),
+]
+    
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # ✅
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
     date = models.DateField()
     description = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='other')
 
     def __str__(self):
         return f"{self.title} - {self.transaction_type} - {self.amount}"
