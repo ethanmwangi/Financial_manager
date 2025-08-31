758f64c5d897083af2b88f90e2685e4a376b77bd
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-08-31 23:06:55 +0300

    redid the models and made migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    # if you want Cloudinary later:
+    # cover = models.ImageField(upload_to='books/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
