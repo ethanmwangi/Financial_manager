21fd6d01c554ccf721dabf8711efb12a8217144e
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-09-03 12:04:24 +0300

    changed the allowed and added it in the env file

--- financial_manager/settings.py
+++ financial_manager/settings.py
@@ -30,11 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS =[
-    'financial-manager-9ko9.onrender.com',
-    'localhost',
-    '127.0.0.1',
-  ]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') 
 
 
 # Application definition
